77fc8c02d1b97ae7efc50d007db29eca62634a3d
syun17 <shunki.s777@gmail.com>
2026-05-17 01:33:05 +0900

    ルーティング作成

---
+++ stamp_rally_front/src/components/Manual.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Manual() {
+  return (
+    <div className="manual">
+      <h2>マニュアル</h2>
+      <p>スタンプラリーの遊び方がここに表示されます。</p>
+    </div>
+    )
+}
+
+export default Manual
---
+++ stamp_rally_front/src/components/Prize.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Prize() {
+  return (
+    <div className="prize">
+      <h2>景品一覧</h2>
+      <p>獲得できる景品がここに表示されます。</p>
+    </div>
+  )
+}
+
+export default Prize
